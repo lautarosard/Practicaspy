fc3042db44ec8d8d1a34d83d62157a6988080f28
Lauta <lautaro37pumm@hotmail.com>
2022-10-26 00:34:28 -0300

    Comitieración a1

---
+++ TP5/TP5_9.py
@@ -0,0 +1,9 @@
+#funci
+def cargararea():
+    print('Cargue la zona que desea')
+    x=int(input('Ingrese el codigo postal: '))
+    t=str(input('Ingrese el area que le pertenece: '))
+    return [x,t]
+#Programa
+print('Presenta y carga localidades')
+local=
